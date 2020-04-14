0bdea2155da72f52c6db31e788e1da2844976c62
haladynkamil <haladyn.kamil@gmail.com>
2020-04-14 12:16:42 +0200

    pierwszy view

--- dayoff/views.py
+++ dayoff/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.apps import apps
 # Create your views here.
 
+class ClientList(TemplateView):
+    title = 'Lista klientów'
+    template_name='clientlist.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['clientlist'] = apps.get_model('dayoff.Client').objects.all()
+        context['title'] = self.title
+        return context
 
--- dayoffcrm/urls.py
+++ dayoffcrm/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dayoff.views import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('lista-klientow/', ClientList.as_view())
 ]
